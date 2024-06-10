cc63f2cff4c36eed3245e8d4bd2684bc8e6f127a
qqadi <qaisalqadi2000@gmail.com>
2024-06-10 10:28:13 +0300

    updated enqueue method

--- src/queue.py
+++ src/queue.py
@@ -3,28 +3,14 @@ class Queue:
         self.que = []
         self.size = 0
         
-    def add_first(self, data):
-        if self.que[0] = None:
+    def en_queue(self, data):
+        if self.que[0] == None:
             self.que[0] = data
             self.size +=1
         else:
             counter = self.size
-            while self.que[counter]:
-                counter -=1
+            while counter != 0:
                 self.que[counter+1] = self.que[counter]
-- - - - - - 
-1 2 3 4 5
-
-t = 2
-
-- - - - - - 
-
-
-temp2 = temp1
-temp1 = counter+1
-counter+1 = 
-counter = temp2
-
-
-
-
+                counter -=1
+            self.que[0] = data
+            self.size +=1
---
+++ src/test_queue.py
@@ -0,0 +1,4 @@
+from queue import Queue
+
+def test_add_first():
+    que = Queue()
